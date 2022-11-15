15512ae82698e1e7012ce3c5cf24ebadc6a119ac
unknown <Sena@MEDAPRCSGFSD745.FormacionSena.RED>
2022-11-15 14:13:47 -0500

    first commit 15/11/2022  14:13

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express') //Importar el paquete express
+const app = express()
+const hbs= require('hbs')//solamente para trabajar a nivel de plantillas
+const port = 8282 //Definir el puerto
+
+//servidor contenido estático
+//Establecer el directorio donde se encuentran los
+//archivos html
+app.use( express.static('public') );
+
+//motor de plantilla
+hbs.registerPartials(__dirname + '/public/views/partials', function (err) {});
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/public/views');
+
+app.get('/', (req,res) => {
+    //res.send('Hola Mundo')
+    res.render('registrarAbono', {
+        
+    })
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Escuchando por el puerto ${port}`)
+})
